3a922dea83d6dbd9f0ccc88d4291877eb70a8ff2
Pravesh Dholwani <70967219+pravesh-dholwani@users.noreply.github.com>
2023-04-11 11:52:56 +0530

    bot.py

---
+++ bot.py
@@ -0,0 +1,17 @@
+import telebot
+import os
+
+# bot_token = os.environ.get("6245614297:AAHSK1fRtjevCo3WnsIQamGEBCX-2vAYQsA")
+
+bot = telebot.TeleBot("6245614297:AAHSK1fRtjevCo3WnsIQamGEBCX-2vAYQsA")
+
+@bot.message_handler(['start' , 'hello'])
+def send_welcome_message(message):
+    bot.reply_to(message , "Welcome to the Trial bot of Pravesh")
+
+@bot.message_handler(func=lambda msg:True)
+def send_to_sender(message):
+    bot.reply_to(message , message.text)
+
+bot.infinity_polling()
+
