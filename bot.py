36598d8daba6934af9947170d056df824c45cbc8
Pravesh Dholwani <70967219+pravesh-dholwani@users.noreply.github.com>
2023-04-17 12:25:05 +0530

    update bot.py

--- bot.py
+++ bot.py
@@ -1,18 +1,41 @@
-import telebot
+# import telebot
+# import os
+
+# # bot_token = os.environ.get("6245614297:AAHSK1fRtjevCo3WnsIQamGEBCX-2vAYQsA")
+
+# bot = telebot.TeleBot("6245614297:AAHSK1fRtjevCo3WnsIQamGEBCX-2vAYQsA")
+
+# @bot.message_handler(['start' , 'hello'])
+# def send_welcome_message(message):
+#     bot.reply_to(message , "Welcome to the Trial bot of Pravesh")
+
+# @bot.message_handler(func=lambda msg:True)
+# def send_to_sender(message):
+#     bot.reply_to(message , message.from_user.username)
+#     bot.reply_to(message , message.text)
+
+# bot.infinity_polling()
+
+
+
 import os
+import telegram
+from telegram.ext import CommandHandler, MessageHandler, Filters, Updater
 
-# bot_token = os.environ.get("6245614297:AAHSK1fRtjevCo3WnsIQamGEBCX-2vAYQsA")
+# TOKEN = os.environ['TELEGRAM_TOKEN']
+bot = telegram.Bot(token="6245614297:AAHSK1fRtjevCo3WnsIQamGEBCX-2vAYQsA")
 
-bot = telebot.TeleBot("6245614297:AAHSK1fRtjevCo3WnsIQamGEBCX-2vAYQsA")
+def start(update, context):
+    context.bot.send_message(chat_id=update.message.chat_id, text="Hello, I'm a bot!")
 
-@bot.message_handler(['start' , 'hello'])
-def send_welcome_message(message):
-    bot.reply_to(message , "Welcome to the Trial bot of Pravesh")
+def echo(update, context):
+    context.bot.send_message(chat_id=update.message.chat_id, text=update.message.text)
 
-@bot.message_handler(func=lambda msg:True)
-def send_to_sender(message):
-    bot.reply_to(message , message.from_user.username)
-    bot.reply_to(message , message.text)
+updater = Updater(token=TOKEN, use_context=True)
+dispatcher = updater.dispatcher
+dispatcher.add_handler(CommandHandler('start', start))
+dispatcher.add_handler(MessageHandler(Filters.text, echo))
 
-bot.infinity_polling()
+updater.start_polling()
+updater.idle()
 
